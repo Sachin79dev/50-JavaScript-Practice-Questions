30e6b63564ca1a5afe98ecda1425f6a2ee05eff2
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-02 17:13:11 +0530

    Add fruit array and print each fruit
    Add fruit array and print each fruit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+// 1. Create an array of 5 fruits and print each one using indexing.
 
+let fruits = ["apple", "banana", "mango", "guava", "kiwi"];
+
+fruits.forEach((n) => {
+    console.log(n);
+})
