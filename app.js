33800d9007513d36b5e9fa850c4d0c604dadf1f6
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-02 17:32:36 +0530

    Add array merging, deduplication, and sorting
    Merged two arrays, removed duplicates, and sorted the result.

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ let fruits = ["apple", "banana", "mango", "guava", "kiwi"];
 fruits.forEach((n) => {
     console.log(n);
 })
+
+
+// 2. Merge two arrays of numbers, remove duplicates, and sort the result in ascending order.
+
+
+let arr1 = [1, 2, 3, 4]
+
+let arr2 = [3, 4, 5, 6]
+
+
+let merged = [...arr1, ...arr2]
+
+let unique = merged.filter((n, i) => {
+    return merged.indexOf(n) == i;
+})
+
+
+console.log(unique.sort()); // (6) [1, 2, 3, 4, 5, 6]
